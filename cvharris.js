27d931b6fe3c20670bfc6aecda5fe0ccba0f514f
AminaAMMOURI <mina.ammouri@gmail.com>
2020-11-23 17:08:09 +0100

    DarkMode

---
+++ cvharris.js
@@ -0,0 +1,23 @@
+
+    ScrollReveal().reveal('.businessman', { duration: 2000 });
+    ScrollReveal().reveal('.car', { interval: 200 });
+
+
+    const btnToggle = document.querySelector('.btn-toggle');
+
+    btnToggle.addEventListener('click', () => {
+
+        const body = document.body;
+
+        if(body.classList.contains('dark')){
+            body.classList.add('light')
+            body.classList.remove('dark')
+            btnToggle.innerHTML = "Go Dark"
+
+        } else if(body.classList.contains('light')){
+            body.classList.add('dark')
+            body.classList.remove('light')
+            btnToggle.innerHTML = "Go Light"
+        }
+            
+    })
